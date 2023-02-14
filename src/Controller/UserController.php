d2c3d84c666abe463383ce240b5a483bcad9b0e4
Mootaz Ben Farhat <mootaz.benfarhat@esprit.tn>
2023-02-14 14:18:19 +0100

    editing templates missing delete user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -1,21 +1,24 @@
 <?php
 
 namespace App\Controller;
-//test
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use App\Repository\UtilisateurRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
+    // #[Route('/user', name: 'app_user')]
+    // public function index(): Response
+    // {
+    //     return $this->render('utilisateur/index.html.twig', [
+    //         'controller_name' => 'UserController',
+    //     ]);
+    // }
 
     #[Route('/backofficemed', name: 'app_medecin')]
     public function home(): Response
@@ -49,6 +52,16 @@ class UserController extends AbstractController
     }
 
 
+    #[Route('/user', name: 'app_utilisateur_index', methods: ['GET'])]
+    public function index3(UtilisateurRepository $utilisateurRepository): Response
+    {
+        return $this->render('utilisateur/index.html.twig', [
+            'utilisateurs' => $utilisateurRepository->findAll(),
+        ]);
+    }
+
+
+
 
 
     #[Route('/logout', name: 'app_logout', methods: ['GET'])]
@@ -56,6 +69,42 @@ class UserController extends AbstractController
     {
         // controller can be blank: it will never be called!
         return $this->redirectToRoute('login/index.html.twig');
+    }
+
+    #[Route('user/{id}', name: 'app_utilisateur_show', methods: ['GET'])]
+    public function show(Utilisateur $utilisateur): Response
+    {
+        return $this->render('utilisateur/show.html.twig', [
+            'utilisateur' => $utilisateur,
+        ]);
+    }
+
+    // #[Route('/{id}/edit', name: 'app_utilisateur_edit', methods: ['GET', 'POST'])]
+    // public function edit(Request $request, Utilisateur $utilisateur, UtilisateurRepository $utilisateurRepository): Response
+    // {
+    //     $form = $this->createForm(UtilisateurType::class, $utilisateur);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $utilisateurRepository->save($utilisateur, true);
+
+    //         return $this->redirectToRoute('app_utilisateur_index', [], Response::HTTP_SEE_OTHER);
+    //     }
+
+    //     return $this->renderForm('utilisateur/edit.html.twig', [
+    //         'utilisateur' => $utilisateur,
+    //         'form' => $form,
+    //     ]);
+    // }
+
+    #[Route('/{id}', name: 'app_utilisateur_delete', methods: ['POST'])]
+    public function delete(Request $request, Utilisateur $utilisateur, UtilisateurRepository $utilisateurRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$utilisateur->getId(), $request->request->get('_token'))) {
+            $utilisateurRepository->remove($utilisateur, true);
+        }
+
+        return $this->redirectToRoute('app_utilisateur_index', [], Response::HTTP_SEE_OTHER);
     }
 }
 
