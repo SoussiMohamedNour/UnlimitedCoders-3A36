83a892d16a3b260ce78cc714913b7c0042c81ab3
Soussi Mohamed Nour <SoussiMohamedNour@noreply-github.com>
2023-02-14 13:09:20 +0100

    base crud and form

---
+++ src/Form/OrdonnanceType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ordonnance;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrdonnanceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('validite')
+            ->add('idconsultation')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Ordonnance::class,
+        ]);
+    }
+}
