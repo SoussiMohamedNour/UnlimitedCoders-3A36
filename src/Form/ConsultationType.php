5f1d7dd7215bdd7450b881cfc5d69fab7172a3fe
Soussi Mohamed Nour <SoussiMohamedNour@noreply-github.com>
2023-02-15 19:39:52 +0100

    Add form CSS and Layout

--- src/Form/ConsultationType.php
+++ src/Form/ConsultationType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Consultation;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,10 @@ class ConsultationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('matriculemedecin')
-            ->add('idpatient')
-            ->add('dateconsultation')
-            ->add('montant')
+            ->add('matriculemedecin',TextType::class,['label'=>'Matricule Médecin','attr'=>['class'=>'form-control','placeholder'=>'Matricule Médecin']])
+            ->add('idpatient',TextType::class,['label'=>'Identifiant Patient','attr'=>['class'=>'form-control','placeholder'=>'Identifiant Patient']])
+            ->add('dateconsultation',DateType::class,['label'=>'Date Consultation','attr'=>['class'=>'form-control']])
+            ->add('montant',IntegerType::class,['attr'=>['class'=>'form-control','placeholder'=>'Montant en DT']])
         ;
     }
 
