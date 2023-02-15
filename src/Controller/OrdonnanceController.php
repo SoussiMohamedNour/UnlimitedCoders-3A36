9d06aaaddc1af0dfa7f59fffc7c82178109bcf43
Soussi Mohamed Nour <SoussiMohamedNour@noreply-github.com>
2023-02-15 19:39:40 +0100

    Fix Typo

--- src/Controller/OrdonnanceController.php
+++ src/Controller/OrdonnanceController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/backoffice/')]
+#[Route('/backoffice')]
 class OrdonnanceController extends AbstractController
 {
     #[Route('/ordonnance', name: 'app_ordonnance_index', methods: ['GET'])]
