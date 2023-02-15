a0412b6eefa01e57bf5ca48318b0ef602d19ac9f
Soussi Mohamed Nour <SoussiMohamedNour@noreply-github.com>
2023-02-15 18:19:40 +0100

    ManyToMany

---
+++ src/Form/ConsultationType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Consultation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConsultationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('matriculemedecin')
+            ->add('idpatient')
+            ->add('dateconsultation')
+            ->add('montant')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Consultation::class,
+        ]);
+    }
+}
---
+++ src/Form/MedicamentType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Medicament;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MedicamentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('dosage')
+            ->add('prix')
+            ->add('description')
+            ->add('ordonnances')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Medicament::class,
+        ]);
+    }
+}
