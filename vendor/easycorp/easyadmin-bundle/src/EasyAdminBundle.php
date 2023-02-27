86b0680e9b0e2fa8d1cc77f0d4f9cc5f5818784e
Soussi Mohamed Nour <SoussiMohamedNour@noreply-github.com>
2023-02-27 21:35:03 +0100

    hide vendor

--- vendor/easycorp/easyadmin-bundle/src/EasyAdminBundle.php
+++ vendor/easycorp/easyadmin-bundle/src/EasyAdminBundle.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class EasyAdminBundle extends Bundle
 {
-    public const VERSION = '4.5.2';
+    public const VERSION = '4.6.0';
 
     public function build(ContainerBuilder $container): void
     {
