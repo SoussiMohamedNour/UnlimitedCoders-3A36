30ebd2602a7d17e7c9faee1c89eff9c12a9de97f
Soussi Mohamed Nour <SoussiMohamedNour@noreply-github.com>
2023-03-04 15:34:42 +0100

    partie back integration arij

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,21 +10,21 @@ use App\dto\Pie;
 
 class DefaultController extends AbstractController
 {
-    // #[Route('/backoffice', name: 'app_default')]
-    // public function index1(): Response
-    // {
-    //     return $this->render('BackOffice/admin.html.twig', [
-    //         'controller_name' => 'DefaultController',
-    //     ]);
+    #[Route('/backoffice', name: 'app_default')]
+    public function index1(): Response
+    {
+        return $this->render('BackOffice/admin.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
 
-    // }
-    // #[Route('/Frontoffice', name: 'home')]
-    // public function index2(): Response
-    //     {
-    //         return $this->render('Frontoffice/base.html.twig', [
-    //             'controller_name' => 'DefaultController',
-    //         ]);
-    //     }
+    }
+    #[Route('/Frontoffice', name: 'home')]
+    public function index2(): Response
+        {
+            return $this->render('Frontoffice/base.html.twig', [
+                'controller_name' => 'DefaultController',
+            ]);
+        }
    
     
 }
